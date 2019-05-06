1b6c52343ae4c126bb2d46beb79e1863f26481c7
AurelWu <aurelwuensch@gmail.com>
2019-05-06 18:08:25 +0200

    initial commit to test if package works

---
+++ Runtime/Scripts/QuadGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections;
+
+namespace Wunderwunsch.QuadMapLibrary
+{ 
+    public static partial class QuadGrid
+    {
+        public static void TestMethod()
+        {
+            NativeList<int2> testList = new NativeList<int2>(Allocator.Temp);
+            testList.Add(new int2(3, 5));
+            GetTiles.AdjacentToTile(new int2(3, 5), testList);
+            Debug.Log(testList[0]);
+            Debug.Log(testList[1]);
+            Debug.Log(testList[2]);
+            Debug.Log(testList[3]);
+            Debug.Log(testList[4]);
+            testList.Dispose();
+        }
+    }
+}
---
+++ Runtime/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wunderwunsch.QuadMapLibrary;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuadGrid.TestMethod();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
